87cba68147be266b53eb75464ad15580ab222d8e
Nachiket <nachiketkhule.nk@gmail.com>
2023-01-22 12:13:41 +0530

    User Service,Dto added

---
+++ AgriStore/src/main/java/com/nk/agri/store/dtos/UserDto.java
@@ -0,0 +1,23 @@
+package com.nk.agri.store.dtos;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+
+    private String userId;
+    private String name;
+    private String email;
+    private String password;
+    private String gender;
+    private String about;
+    private String imageName;
+
+}
---
+++ AgriStore/src/main/java/com/nk/agri/store/services/UserService.java
@@ -0,0 +1,27 @@
+package com.nk.agri.store.services;
+
+import com.nk.agri.store.controller.User;
+import com.nk.agri.store.dtos.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+
+    //Create
+    UserDto createUser(UserDto user);
+
+    //update
+    UserDto updateUser(UserDto user, String userId);
+    //delete
+    void deleteUser(String userId);
+    //getAll user
+    List<UserDto> getAllUSer();
+    //get single by id
+    UserDto getUserById(String userID);
+    //get by email
+    UserDto getUserByEmail(String email);
+    //search user
+    List<UserDto> searUser(String keyword);
+    //other user specific features
+
+}
