abd138662fbc47a412fdd807d1ca8199ed7589ae
Nachiket <nachiketkhule.nk@gmail.com>
2023-01-22 11:34:45 +0530

    Spring template added with the DB config

---
+++ AgriStore/src/main/java/com/nk/agri/store/AgriStoreApplication.java
@@ -0,0 +1,13 @@
+package com.nk.agri.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriStoreApplication.class, args);
+	}
+
+}
---
+++ AgriStore/src/test/java/com/nk/agri/store/AgriStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nk.agri.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
