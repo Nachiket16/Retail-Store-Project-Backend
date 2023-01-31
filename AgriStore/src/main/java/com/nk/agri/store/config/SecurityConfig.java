3e23bc15979c8e71456b9f72bc1ee6633c5d1ee9
Nachiket <nachiketkhule.nk@gmail.com>
2023-01-31 11:29:27 +0530

    IN MEMORY CONFIGURATION

--- AgriStore/src/main/java/com/nk/agri/store/config/SecurityConfig.java
+++ AgriStore/src/main/java/com/nk/agri/store/config/SecurityConfig.java
@@ -5,21 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig
 {
+
+    // IN MEMORY CONFIGURATION
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails admin  = User.builder()
                 .username("Nachiket")
-                .password("admin")
+                .password(passwordEncoder().encode("admin"))
                 .roles("ADMIN")
                 .build();
         UserDetails normal = User.builder()
                 .username("Aniket")
-                .password("admin")
+                .password(passwordEncoder().encode("admin"))
                 .roles("NORMAL")
                 .build();
         //Users create
@@ -27,6 +31,12 @@ public class SecurityConfig
 
                 return new InMemoryUserDetailsManager(admin,normal);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
 
 }
